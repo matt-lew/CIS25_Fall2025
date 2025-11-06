eb58d97b0987c24f25765b56a087df8542e903cf
matt-lew <matthewlew6@gmail.com>
2025-11-05 16:37:06 -0800

    Week 4 debug

--- main/main.cpp
+++ main/main.cpp
@@ -1,36 +1,28 @@
 // main.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <iostream> 
 using namespace std;
+int main() {
+	//int choice;
+	//cout << "1. Add Item\n2. View Items\n3. Exit\n";
+	//cout << "Choose an option: ";
+	//cin >> choice;
 
-int main()
-{
-    string itemName;
-    int quantity;
-    float cost;
+	//if (choice == 1) cout << "Item added!\n";
+	//else if (choice == 2) cout << "Displaying items...\n";
+	//else if (choice == 3) cout << "Exiting...\n";
+	//else cout << "Invalid option.\n";
+	//return 0;
 
-    cout << "Hello Inventory System!\n";
-    
-    cout << "Enter item name: \t";
-    cin >> itemName;
-    cout << "Enter quantity: \t";
-    cin >> quantity;
-    cout << "Enter cost: \t\t$";
-    cin >> cost;
+	float n1, n2, n3;
 
-    cout << "Total cost for " << itemName << ": $" << (quantity * cost) << endl;
-    return 0;
+	cout << "Enter three numbers: \t";
+	cin >> n1 >> n2 >> n3;
+	if (n1 >= n2 && n1 >= n3) cout << "Largest number: \t" << n1;
+	else if (n2 >= n1 && n2 >= n3) cout << "Largest number: \t" << n2;
+	else if (n3 >= n1 && n3 >= n2) cout << "Largest number: \t" << n3;
+	return 0;
+
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
